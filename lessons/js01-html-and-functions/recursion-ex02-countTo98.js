88d7e95d2211d0adf2e14b8b7c4a4f9e2e121661
Daniel Thalhuber <dev@uhf.li>
2020-03-20 09:38:27 -0700

    lesson - js1: html & functions: recursion: exercise 2: add interviewing steps

--- lessons/js01-html-and-functions/recursion-ex02-countTo98.js
+++ lessons/js01-html-and-functions/recursion-ex02-countTo98.js
@@ -1,12 +1,44 @@
+/*
+  Goal:
+
+  - Write a function called `countTo98` that calls `console.log` once for every number from 0 to 98
+
+  Examples:
+
+  - Call `countTo98()` and observe the numbers 0 to 98 logged
+  - Call `countTo98(98)` and observe the number 98 logged
+  - Call `countTo98(99)` and observe that nothing is logged
+
+  Signature:
+
+  - Parameters: only an internal/private `counter` initialized to 0
+  - Return: `undefined`
+  - Side effect: logs to the console
+
+  Code:
+*/
 const countTo98 = (count = 0) => {
-  if (count >= 99) return;
+  if (count > 98) return;
 
   console.log(count);
   return countTo98(count + 1);
 };
 
-countTo98();
+/*
+  Test:
+*/
+const test = () => {
+  console.log('\nshould log the numbers 0 to 98');
+  countTo98();
+
+  console.log('\nshould log the number 98');
+  countTo98(98);
+
+  console.log('\nshould log nothing');
+  countTo98(99);
+};
 
 module.exports = {
-  countTo98,
+  code: countTo98,
+  test,
 };
