6946a00068babd977b4d760ddbcdc2c4a77d5718
Daniel Thalhuber <dev@uhf.li>
2020-03-20 11:01:20 -0700

    lesson - js1: html & functions: recursion: exercise 3: add interviewing steps

--- lessons/js01-html-and-functions/recursion-ex03-count-to-me.js
+++ lessons/js01-html-and-functions/recursion-ex03-count-to-me.js
@@ -1,11 +1,55 @@
+/*
+  Goal:
+
+  - Write a function called `countToMe` that `console.log`s every number from 8 to the input number
+
+  Examples:
+
+  - Call `countToMe()` and observe no logging (returns `undefined`)
+  - Call `countToMe(7)` and observe no logging
+  - Call `countToMe(8)` and observe that 8 is logged
+  - Call `countToMe(12)` and observe that the numbers 8 through 12 are logged
+
+  Signature:
+
+  - Parameters:
+    - `limit`, a number initialized to 0, which indicates how far to count
+    - `count`, a private variable initialized to 0, used to track iterations
+
+  Explanation:
+
+  - Start with parameters `limit` and `count` initialized to 0 and 8 respectively
+  - Base case: if `count` > `limit`, return/stop
+  - Recursive case:
+     - Log `count`
+     - Return a call to `countToMe` incrementing count by 1
+
+  Code:
+*/
 const countToMe = (limit = 0, count = 8) => {
   if (count > limit) return;
   console.log(count);
   return countToMe(limit, count + 1);
 };
 
-countToMe(10);
+/*
+  Test:
+*/
+const test = () => {
+  console.log('\nshould log nothing');
+  countToMe();
+
+  console.log('\nshould log nothing');
+  countToMe(7);
+
+  console.log('\nshould log 8');
+  countToMe(8);
+
+  console.log('\nshould log the numbers 8 through 12');
+  countToMe(12);
+};
 
 module.exports = {
-  countToMe,
+  code: countToMe,
+  test,
 };
